499e94bfe2aa3e34caabf0374418dbd6f61df0b4
Dilshani Senavirathna <141391107+Dilshani16@users.noreply.github.com>
2025-04-05 19:02:34 +0530

    initial commit

---
+++ drycraft/src/main/java/com/drycraft/drycraft/DrycraftApplication.java
@@ -0,0 +1,13 @@
+package com.drycraft.drycraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrycraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrycraftApplication.class, args);
+	}
+
+}
---
+++ drycraft/src/test/java/com/drycraft/drycraft/DrycraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drycraft.drycraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrycraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
